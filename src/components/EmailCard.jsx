99824d4fa188288b418a7c76df7e20da9c5451ca
SIMPLYxlr8 <actually.makhd@gmail.com>
2025-02-10 23:27:02 +0530

    email card component added

--- src/components/EmailCard.jsx
+++ src/components/EmailCard.jsx
@@ -1,7 +1,28 @@
 function EmailCard(){
     return (
         <>
+            <a href="#" className="list-group-item list-group-item-action active py-3 lh-tight" aria-current="true">
+                <div className="d-flex w-100 align-items-center justify-content-between">
+                    <strong className="mb-1">List group item heading</strong>
+                    <button className="btn" title="Delete">
+                        <i className="bi bi-trash"></i>
+                    </button>
+                </div>
+                <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.
+                </div>
+            </a>
+            <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                <div className="d-flex w-100 align-items-center justify-content-between">
+                    <strong className="mb-1">List group item heading</strong>
+                    <button className="btn" title="Delete">
+                        <i className="bi bi-trash"></i>
+                    </button>
+                </div>
+                <div className="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.
+                </div>
+            </a>
         </>
     )
 }
+
 export default EmailCard;
--- src/components/EmailListingContainer.jsx
+++ src/components/EmailListingContainer.jsx
@@ -1,7 +1,10 @@
+import EmailCard from "./EmailCard.jsx";
+
 function EmailListingContainer(){
     return(
         <>
             <div className="container email-list w-25">
+                {/* comment - heading for email list with email count.*/}
                 <div className="inbox">
                     <div className="d-flex align-items-center justify-content-between">
                         <div className="ms-3">
@@ -11,6 +14,10 @@ function EmailListingContainer(){
                             <span className="badge text-light">14</span>
                         </div>
                     </div>
+                </div>
+                {/* comment - inbox container.*/}
+                <div className="list-group list-group-flush scrollarea">
+                    <EmailCard/>
                 </div>
             </div>
         </>
